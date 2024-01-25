ce7ddf23466cdbf65a417442f19bb0a22976da50
sabalza <maryori.sabalza.ext@nokia.com>
2024-01-25 14:41:07 -0500

    [ADD] Function to send data to LUMU

--- dns_collector.py
+++ dns_collector.py
@@ -8,7 +8,7 @@ import requests
 
 def dns_file_parse(filename):
     """
-        Method to open and read the queries file 
+        Method to open file, read the queries file and parser
     """
     data = []
 
@@ -19,6 +19,33 @@ def dns_file_parse(filename):
                 ip_client, host, port = p
                 data.append((ip_client,host))
     return data
+
+def send2_Lumu(chunk):
+    api_lumu = "https://api.lumu.io/collectors/custom-collectors/collector-id/send-dns-queries"
+    lumu_client_key = "d39a0f19-7278-4a64-a255-b7646d1ace80"
+    collector_id = "5ab55d08-ae72-4017-a41c-d9d735360288 "
+
+    headers = {
+        "Content-Type": "application/json",
+        "Lumu-Client-Key": lumu_client_key
+    }
+
+    payload = {
+        "Collector-ID": collector_id,
+        "queries_dns": chunk
+    }
+
+    try:
+        response = requests.post(api_lumu, json=payload, headers=headers)
+
+        if response.status_code == 200:
+            print("Data was sent successfully")
+        else:
+            print("Error sending data to Lumu Technologies %s" % response.status_code)
+    except Exception as e:
+        print(f"Error Connection: {e}")
+
+
 
 if __name__ == "__main__":
     filename = "queries"
